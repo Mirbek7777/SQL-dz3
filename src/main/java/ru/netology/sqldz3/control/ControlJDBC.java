94dbf6ced0df21624ef84d2720c6ff6082335bb1
Mirbek <mirbek_karimberdiev@mail.ru>
2023-11-14 15:41:12 +0300

    2th commit

--- src/main/java/ru/netology/sqldz3/control/ControlJDBC.java
+++ src/main/java/ru/netology/sqldz3/control/ControlJDBC.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.netology.sqldz3.mapper.Products;
 import ru.netology.sqldz3.repository.RepositoryJDBC;
 
 import java.util.List;
@@ -19,11 +18,11 @@ public class ControlJDBC {
     }
 
     @GetMapping("/products/fetch-product")
-    public List<Products> getSqlSetup(@RequestParam String name) {
+    public List<String> getSqlSetup(@RequestParam String name) {
         return repositoryJDBC.getProductName(name);
     }
     @PostMapping("/post")
-    public List<Products> getSqlSetups(@RequestParam String name) {
+    public List<String> getSqlSetups(@RequestParam String name) {
         return repositoryJDBC.getProductName(name);
     }
 }
--- src/main/java/ru/netology/sqldz3/repository/RepositoryJDBC.java
+++ src/main/java/ru/netology/sqldz3/repository/RepositoryJDBC.java
@@ -5,31 +5,30 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
-import ru.netology.sqldz3.mapper.Products;
-import ru.netology.sqldz3.mapper.RowMapperProducts;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Repository
 public class RepositoryJDBC {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private String script;
 
     public RepositoryJDBC(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
-    public List<Products> getProductName(String name) {
+    public List<String> getProductName(String name) {
 
         String sql = read("myScript.sql");
         SqlParameterSource parameterSource = new MapSqlParameterSource("name", name);
-        return new ArrayList<>(namedParameterJdbcTemplate.query(sql, parameterSource, new RowMapperProducts()));
+        return namedParameterJdbcTemplate.queryForList(script, Map.of("name", name), String.class);
 
         //String query = "select product_name from netology.orders join netology.customers on customers.id = customer_id where name = 'Николай'";
         //List<Products> productsList = namedParameterJdbcTemplate.query(query, new MapSqlParameterSource(), new RowMapperProducts());
